ba707d584d1d0652ca08e4f10ef09bf58515c5b0
alifeee <alfie@biscuits.digital>
2024-02-14 10:50:11 +0000

    update docker image and silence npm update warnings in build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build with npm via docker
 
-docker run --rm --workdir /_site -v $(pwd):/_site --entrypoint "npm" node:18.16.1-alpine3.18 run build
+docker run --rm --workdir /_site -v $(pwd):/_site --entrypoint "npm" node:current-alpine3.19 run build --no-update-notifier
